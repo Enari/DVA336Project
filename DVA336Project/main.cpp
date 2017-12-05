9fba22585f033c92901611b69560928ef123b446
Anton Roslund <enari@minit.nu>
2017-12-05 09:34:00 +0100

    Initial Commit

---
+++ DVA336Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DVA336Project
+//
+//  Created by Anton Roslund on 2017-12-05.
+//  Copyright © 2017 Anton Roslund. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
